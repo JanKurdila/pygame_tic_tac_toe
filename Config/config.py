56a99567c011a84bf8da434b91885d6e5deffc30
JanKurdila <lektorpythonav@gmail.com>
2024-05-25 16:07:09 +0200

    2) detekcia na kliknutie myšou a umiestňovanie znakov na dosku

--- Config/config.py
+++ Config/config.py
@@ -1,3 +1,7 @@
 import pygame
 
 ROZLISENIE = (300, 300)
+
+POZADIE = pygame.Color(255, 255, 255)
+
+FPS = 30
--- main.py
+++ main.py
@@ -13,14 +13,22 @@ def setup_game(window):
     o_img = pygame.transform.scale(o_img, (80, 80))
     board_img = pygame.transform.scale(board_img, config.ROZLISENIE)
 
-
     # Vyplnenie pozadia
-    window.fill((255, 255, 255))  # Biele pozadie
+    window.fill(config.POZADIE)  # Biele pozadie
 
     # Zobrazenie hracej dosky
     window.blit(board_img, (0, 0))
 
-    return x_img, o_img
+    return x_img, o_img, board_img
+
+def draw_figures(window, board, x_img, o_img):
+    "Interakcia s používateľom - detekcia na kliknutie myšou a umiestňovanie znakov na dosku"
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == 'X':
+                window.blit(x_img, (col * 100 + 10, row * 100 + 10))
+            elif board[row][col] == 'O':
+                window.blit(o_img, (col * 100 + 10, row * 100 + 10))
 
 if __name__ == "__main__":
     pygame.init()
@@ -28,7 +36,14 @@ if __name__ == "__main__":
     pygame.display.set_caption("Piškvorky")
 
     # Nastavenie hry
-    x_img, o_img = setup_game(window)
+    x_img, o_img, board_img = setup_game(window)
+
+    # Inicializácia hracej dosky
+    board = [[None]*3 for _ in range(3)]
+    to_move = 'X'
+
+    # Inicializácia Clock
+    clock = pygame.time.Clock()
 
     while True:
         # Ak vypnem okno, musím vypnuť pygame
@@ -37,4 +52,24 @@ if __name__ == "__main__":
                 pygame.quit() # Vypnutie pygamu
                 sys.exit() # Vypnutie celého programu
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouseX, mouseY = event.pos
+                clicked_row = mouseY // 100
+                clicked_col = mouseX // 100
+
+                if board[clicked_row][clicked_col] is None:
+                    board[clicked_row][clicked_col] = to_move
+                    to_move = 'O' if to_move == 'X' else 'X'
+
+        # Vyplnenie pozadia a zobrazenie hracej dosky
+        window.fill(config.POZADIE)
+        window.blit(board_img, (0, 0))
+
+        # Vykreslenie X a O
+        draw_figures(window, board, x_img, o_img)
+
+        # Aktualizácia obrazovky
         pygame.display.update()
+
+        # Obmedzenie snímkovej frekvencie
+        clock.tick(config.FPS)
