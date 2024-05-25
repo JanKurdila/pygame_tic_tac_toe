a4dfed80c37b26907db2551aa782f6b968ca5afb
JanKurdila <lektorpythonav@gmail.com>
2024-05-25 15:52:01 +0200

    1) Načítanie obrázkov a ich vykreslenie

--- main.py
+++ main.py
@@ -2,10 +2,33 @@ import pygame
 from Config import config
 import sys
 
+def setup_game(window):
+    # Načítanie obrázkov
+    board_img = pygame.image.load("Obrázky/Board.png")
+    x_img = pygame.image.load("Obrázky/X.png")
+    o_img = pygame.image.load("Obrázky/O.png")
+
+    # Zmena veľkosti obrázkov X a O (voliteľné, ak chcete zmeniť ich veľkosť)
+    x_img = pygame.transform.scale(x_img, (80, 80))
+    o_img = pygame.transform.scale(o_img, (80, 80))
+    board_img = pygame.transform.scale(board_img, config.ROZLISENIE)
+
+
+    # Vyplnenie pozadia
+    window.fill((255, 255, 255))  # Biele pozadie
+
+    # Zobrazenie hracej dosky
+    window.blit(board_img, (0, 0))
+
+    return x_img, o_img
+
 if __name__ == "__main__":
     pygame.init()
     window = pygame.display.set_mode(config.ROZLISENIE)
     pygame.display.set_caption("Piškvorky")
+
+    # Nastavenie hry
+    x_img, o_img = setup_game(window)
 
     while True:
         # Ak vypnem okno, musím vypnuť pygame
